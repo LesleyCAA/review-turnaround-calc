6339fe3292c32f56a4b3802fdcab50e7392c91dd
Josh Jacobs <joshalexjacobs@gmail.com>
2023-06-14 15:51:38 -0400

    updated note

--- src/App.js
+++ src/App.js
@@ -119,10 +119,31 @@ function App() {
               </span>
             </p>
             <p className="text-sm italic">
-              Note: Papers submitted after 5:00 pm EST or on weekends are
-              recognized as “received” on the following business day. Please
-              plan accordingly, as this may impact your turnaround time.
-              Lesley-observed holidays may also impact this estimate.
+              Note: This date is an estimate. The actual return date is subject
+              to tutor availability and will be determined by the Online
+              Tutoring Manager.
+            </p>
+
+            <p className="text-sm italic">
+              Papers submitted on weekdays after 5:00 pm EST, weekends, or
+              Lesley-observed holidays are recognized as “received” on the
+              following business day. Please plan accordingly, as this may
+              impact your turnaround time.
+            </p>
+
+            <p className="text-sm italic">
+              Please note that this review turnaround calculator does not
+              account for Lesley-observed holidays, which may delay your return
+              date. Consult the{" "}
+              <a
+                className=" font-semibold text-blue-600 hover:text-blue-900 underline"
+                target="_blank"
+                href="https://lesley.edu/faculty-staff/human-resources/university-holidays"
+              >
+                University Holidays
+              </a>{" "}
+              listing to check if a holiday occurs between your submission date
+              and estimated return date.
             </p>
           </div>
         </section>
